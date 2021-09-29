f8c83ba6b1e45025fce34a31c3842a3865cfa60d
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2021-09-29 10:15:26 +0700

    update tema aplikasi, login activity dan menambahkan layout beranda

--- app/src/main/java/com/example/hyponic/MainActivity.java
+++ app/src/main/java/com/example/hyponic/MainActivity.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private EditText edtPassword,edtEmail;
+    private Button btnMasuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        edtPassword = findViewById(R.id.passwordText);
+        edtEmail= findViewById(R.id.emailText);
+        btnMasuk =findViewById(R.id.loginButton);
+
+
+    }
     public void onClickDaftar(View view) {
-        Intent intent = new Intent(this,RegisterActivity.class);
-        startActivity(intent);
+        Intent moveIntent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(moveIntent);
+    }
+    public void onClickMasuk(View view) {
+        Intent moveIntent = new Intent(MainActivity.this, BerandaActivity.class);
+        startActivity(moveIntent);
     }
 }
