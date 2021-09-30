a1f332d4707cf85a7a1a77b1d5cc0ae2dccf5104
dadang17 <iskandardzulqornain38@gmail.com>
2021-09-30 14:44:35 +0700

    create layout register

--- app/src/main/java/com/example/hyponic/RegisterActivity.java
+++ app/src/main/java/com/example/hyponic/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.hyponic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onClickDaftar(View view) {
+        Intent moveIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(moveIntent);
+    }
+    public void onClickMasuk(View view) {
+        Intent moveIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(moveIntent);
     }
 }
