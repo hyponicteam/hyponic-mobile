1259795e0386c967e2af9de17b63130909ced161
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2021-09-30 23:18:30 +0700

    add view artikel layout

--- app/src/main/java/com/example/hyponic/BerandaActivity.java
+++ app/src/main/java/com/example/hyponic/BerandaActivity.java
@@ -2,7 +2,9 @@ package com.example.hyponic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BerandaActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BerandaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
+    }
+
+    public void onClickBacaArtikel(View view) {
+        Intent intent = new Intent(BerandaActivity.this,LihatArtikelActivity.class);
+        startActivity(intent);
     }
 }
