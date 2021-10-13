a99461e5ec08e85da9f8a4da0ed6928ee47c3cab
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2021-10-13 23:45:35 +0700

    change artikel recycle view at home page

---
+++ app/src/main/java/com/example/hyponic/LihatArtikelActivity.java
@@ -0,0 +1,16 @@
+package com.example.hyponic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hyponic.R;
+
+public class LihatArtikelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_artikel);
+    }
+}
--- app/src/main/java/com/example/hyponic/MainActivity.java
+++ app/src/main/java/com/example/hyponic/MainActivity.java
@@ -10,28 +10,41 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity{
     private EditText edtPassword,edtEmail;
-    private Button btnMasuk;
+    private Button btnLogin;
+    private String email, password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init();
+    }
+    public void init(){
         edtPassword = findViewById(R.id.passwordText);
         edtEmail= findViewById(R.id.emailText);
-        btnMasuk =findViewById(R.id.loginButton);
-
-
+        btnLogin =findViewById(R.id.loginButton);
     }
     public void onClickDaftar(View view) {
         Intent moveIntent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(moveIntent);
     }
     public void onClickMasuk(View view) {
-        Intent moveIntent = new Intent(MainActivity.this, BerandaActivity.class);
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+
+        Intent moveIntent = new Intent(this, BerandaActivity.class);
         startActivity(moveIntent);
+
     }
 }
