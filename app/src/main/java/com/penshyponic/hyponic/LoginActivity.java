789571159b5b736009b9519a4a7429a5e4221936
syiida <rosyiidahasnaa@gmail.com>
2021-12-26 11:09:27 +0700

    menambahkan button login sebagai guest

--- app/src/main/java/com/penshyponic/hyponic/LoginActivity.java
+++ app/src/main/java/com/penshyponic/hyponic/LoginActivity.java
@@ -20,7 +20,6 @@ import static com.penshyponic.hyponic.constant.ApiConstant.BASE_URL;
 
 public class LoginActivity extends AppCompatActivity{
     private ActivityLoginBinding binding;
-    private String email, password;
     SharedPrefManager preferences;
 
     @Override
@@ -35,6 +34,9 @@ public class LoginActivity extends AppCompatActivity{
                     .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             finish();
         }
+        binding.loginAsGuest.setOnClickListener(v->{
+            loginApi("ivan@aulia.com","ivanaulia");
+        });
     }
     public void init(){
         preferences = new SharedPrefManager(this);
@@ -45,65 +47,64 @@ public class LoginActivity extends AppCompatActivity{
         startActivity(moveIntent);
     }
     public void onClickMasuk(View view) {
-        email = binding.emailText.getText().toString();
-        password =binding.passwordText.getText().toString();
-
+        String email = binding.emailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
         if(email.equals("")|| password.equals("")){
             binding.emailText.setError("email tidak boleh kosong");
             binding.passwordText.setError("Password tidak boleh kosong");
         }else{
-            showLoading(true);
-            AndroidNetworking.post(BASE_URL+"auth/login")
-                    .addHeaders("Accept", "application/json")
-                    .addBodyParameter("email", email)
-                    .addBodyParameter("password",password)
-                    .setTag("test")
-                    .setPriority(Priority.MEDIUM)
-                    .build()
-                    .getAsJSONObject(new JSONObjectRequestListener() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            showLoading(false);
-                            Log.d("TAG", String.valueOf(response));
-                            try {
-                                if(response.getJSONObject("meta").getString("status").equals("success")){
-                                    JSONObject dataUser = response.getJSONObject("data").getJSONObject("user");
-                                    String token = response.getJSONObject("data").getString("access_token");
-
-                                    Toast.makeText(getApplicationContext(),response.getJSONObject("meta")
-                                            .getString("message"),Toast.LENGTH_SHORT).show();
-
-                                    preferences.saveSPString(preferences.SP_NAMA,dataUser.getString("name"));
-                                    preferences.saveSPString(preferences.SP_EMAIL,dataUser.getString("email"));
-                                    preferences.saveSPInt(String.valueOf(preferences.SP_ID),dataUser.getInt("id"));
-                                    preferences.saveSPString(preferences.SP_TOKEN,token);
-                                    preferences.saveSPBoolean(preferences.SP_IS_LOGIN,true);
-                                    preferences.saveSPString(preferences.SP_Created_At,dataUser.getString("created_at"));
-                                    preferences.saveSPString(preferences.SP_Edited_At,dataUser.getString("updated_at"));
-
-                                    Intent moveIntent = new Intent(getApplicationContext(),MainActivity.class);
-                                    startActivity(moveIntent);
-                                    finish();
-                                }else{
-                                    Toast.makeText(getApplicationContext(),"Login Gagal",Toast.LENGTH_SHORT).show();
-                                }
-
-                            }catch (JSONException e){
-                                Toast.makeText(getApplicationContext(),"Login Gagal"+e,Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        @Override
-                        public void onError(ANError error) {
-                            showLoading(false);
-                            // handle error
-                            Toast.makeText(getApplicationContext(),"Email atau password tidak sesuai", Toast.LENGTH_SHORT).show();
-                            Log.d("ERROR", String.valueOf(error));
-                        }
-                    });
-            //showLoading(false);
-            //Intent moveIntent = new Intent(getApplicationContext(),Home1Activity.class);
-            //startActivity(moveIntent);
+            loginApi(email,password);
         }
+    }
+    public void loginApi(String email,String password){
+        showLoading(true);
+        AndroidNetworking.post(BASE_URL+"auth/login")
+                .addHeaders("Accept", "application/json")
+                .addBodyParameter("email", email)
+                .addBodyParameter("password",password)
+                .setTag("test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        showLoading(false);
+                        Log.d("TAG", String.valueOf(response));
+                        try {
+                            if(response.getJSONObject("meta").getString("status").equals("success")){
+                                JSONObject dataUser = response.getJSONObject("data").getJSONObject("user");
+                                String token = response.getJSONObject("data").getString("access_token");
+
+                                Toast.makeText(getApplicationContext(),response.getJSONObject("meta")
+                                        .getString("message"),Toast.LENGTH_SHORT).show();
+
+                                preferences.saveSPString(preferences.SP_NAMA,dataUser.getString("name"));
+                                preferences.saveSPString(preferences.SP_EMAIL,dataUser.getString("email"));
+                                preferences.saveSPInt(String.valueOf(preferences.SP_ID),dataUser.getInt("id"));
+                                preferences.saveSPString(preferences.SP_TOKEN,token);
+                                preferences.saveSPBoolean(preferences.SP_IS_LOGIN,true);
+                                preferences.saveSPString(preferences.SP_Created_At,dataUser.getString("created_at"));
+                                preferences.saveSPString(preferences.SP_Edited_At,dataUser.getString("updated_at"));
+
+                                Intent moveIntent = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(moveIntent);
+                                finish();
+                            }else{
+                                Toast.makeText(getApplicationContext(),"Login Gagal",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }catch (JSONException e){
+                            Toast.makeText(getApplicationContext(),"Login Gagal"+e,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        showLoading(false);
+                        // handle error
+                        Toast.makeText(getApplicationContext(),"Email atau password tidak sesuai", Toast.LENGTH_SHORT).show();
+                        Log.d("ERROR", String.valueOf(error));
+                    }
+                });
     }
     private void showLoading(Boolean isLoading) {
         if (isLoading) {
